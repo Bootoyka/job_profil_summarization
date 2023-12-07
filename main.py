2b58f4ae2c6af34b174ea81a777923e8ddd6c306
Ju <julien.hourcadette@live.fr>
2023-12-07 19:02:45 +0100

    main.py: add job offers & candidate code from serpapi

--- main.py
+++ main.py
@@ -1,22 +1,51 @@
 from serpapi import GoogleSearch
 import os
 from dotenv import load_dotenv
+import argparse
 
-def test_serp():
-    api_key = os.getenv("SERPAPI_PRIVATE_KEY")
+def job_offer_research(query, api_key):
     params = {
-        "engine": "google_jobs",
-        "q": "Java Developer",
-        "ltype": "1",
-        "hl": "en",
-        "api_key": f"{api_key}"
+        'engine': 'google_jobs',
+        'q': query,
+        'api_key': api_key,
     }
 
     search = GoogleSearch(params)
     results = search.get_dict()
-    jobs_results = results["jobs_results"]
-    print(jobs_results)
+    for job_result in results.get('jobs_results', []):
+        print(f"Title: {job_result.get('title')}")
+        print(f"Company: {job_result.get('company', 'Google Inc.')}\n")
+        print(f"Location: {job_result.get('location')}")
+        print(f"Description: {job_result.get('snippet')}")
+        print("\n" + "=" * 50 + "\n")
+
+def candidates_research(query, api_key):
+    params = {
+        'q': query,
+        'api_key': api_key,
+    }
+
+    search = GoogleSearch(params)
+    results = search.get_dict()
+
+    for organic_result in results.get('organic_results', []):
+        print(f"Title: {organic_result.get('title')}")
+        print(f"URL: {organic_result.get('link')}")
+        print(f"Description: {organic_result.get('snippet')}")
+        print("\n" + "=" * 50 + "\n")
 
 if __name__ == '__main__':
     load_dotenv()
-    test_serp()
+    api_key = os.getenv("SERPAPI_PRIVATE_KEY")
+    query = 'site:linkedin.com/in/ AND "Python developer"'
+    parser = argparse.ArgumentParser(description='Google researches for candidates or offers.')
+    parser.add_argument('type', choices=['candidate', 'offer'], help='What kind of research (candidate or offer)')
+    args = parser.parse_args()
+
+
+    if args.type == 'candidate':
+        candidates_research(query, api_key)
+    elif args.type == 'offer':
+        job_offer_research(query, api_key)
+    else:
+        print("Type must be 'candidate' or 'offer'")
